1f121eef45c875458c1d0598e82526c2e9879406
Gatis Meikulis <gmeikulis@evolution.com>
2022-11-14 01:15:14 +0200

    massive refactor

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,18 +5,22 @@ object Dependencies {
   val ZioHttpVersion = "2.0.0-RC7"
 
   object ZIO {
-//    val Std = "dev.zio" %% "zio" % ZioVersion
+    val Std = "dev.zio" %% "zio" % ZioVersion
 //    val Streams = "dev.zio" %% "zio-streams" % ZioVersion
 //    val test = "dev.zio" %% "zio-test" % ZioVersion % Test
 //    val testSbt = "dev.zio" %% "zio-test-sbt" % ZioVersion % Test
     val Http = "io.d11" %% "zhttp" % ZioHttpVersion
   }
 
-  val AkkaVersion = "2.6.9"
+  val AkkaVersion = "2.7.0"
 
   object Akka {
     val ActorTyped = "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion
     val PersistenceTyped = "com.typesafe.akka" %% "akka-persistence-typed" % AkkaVersion
+    val PersistenceCassandra = "com.typesafe.akka" %% "akka-persistence-cassandra" % "1.1.0"
+
   }
+
+  val LogbackClassic = "ch.qos.logback" % "logback-classic" % "1.4.4"
 
 }
